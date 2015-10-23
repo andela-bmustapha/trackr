775a79ee93d160205bc7cecc4f01a840eb518936
andela-bmustapha <mustapha.babtunde@andela.co>
2015-10-23 18:15:02 +0100

    Adds toolbar to settings

--- app/src/main/java/com/bmustapha/trackr/activities/TrackrSettingsActivity.java
+++ app/src/main/java/com/bmustapha/trackr/activities/TrackrSettingsActivity.java
@@ -1,21 +1,45 @@
 package com.bmustapha.trackr.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.bmustapha.trackr.R;
 import com.bmustapha.trackr.fragments.SettingsFragment;
 
-public class TrackrSettingsActivity extends Activity {
+public class TrackrSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_action_arrow_left);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         getFragmentManager().beginTransaction().replace(R.id.settings_container, new SettingsFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
